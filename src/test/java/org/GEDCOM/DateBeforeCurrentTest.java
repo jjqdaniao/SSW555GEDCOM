9beee62fc3ecc11fb18f7dcba0461df84e71da80
Jing <yjing3@stevens.edu>
2019-03-06 19:12:10 -0500

    Initial Update

---
+++ src/test/java/org/GEDCOM/DateBeforeCurrentTest.java
@@ -0,0 +1,19 @@
+package org.GEDCOM;
+
+import org.gedcom4j.exception.GedcomParserException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.GEDCOM.DateBeforeCurrent.beforeCurrent;
+import static org.junit.Assert.*;
+
+public class DateBeforeCurrentTest {
+
+    @Test
+    public void beforeCurrentTest() throws IOException, GedcomParserException {
+        ArrayList list = new ArrayList();
+        assertEquals(list, beforeCurrent("src/resources/GEDCOMsourcefile/bronte.ged"));
+    }
+}
